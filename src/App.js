b057451e5946edb134b8c091308c072a709050c3
arihanv <arihanvaranasi@gmail.com>
2023-06-03 00:17:44 -0500

    Initial Files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-8xl tracking-tight font-bold">
+          Welcome to Sign Ease!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="mt-5">Learn Sign Language Easily</a>
       </header>
     </div>
   );
